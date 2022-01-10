6fba281a87769169c5e55bd1b362c05ea654c61a
ayaokami <bbaqatb@gmail.com>
2022-01-10 16:14:33 +0900

    新規登録できる場合の実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "すべての項目が正しく入力できれば登録できる" do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できない場合' do
+      it "nicknameが空だと登録できない" do
+      end
+      it "emailが空では登録できない" do
+      end
+      it "登録済みのemailと重複するものは登録できない" do
+      end
+      it "@を含まないemailは登録できない" do
+      end
+      it "passwordが空だと登録できない" do
+      end
+      it "passwordは6文字以上でないと登録できない" do
+      end
+      it "passwordは半角英数字混合でないと登録できない" do
+      end
+      it "passwordとpassword_confirmationが一致しないと登録できない" do
+      end
+      it "last_nameが空だと登録できない" do
+      end
+      it "first_nameが空では登録できない" do
+      end
+      it "last_nameは漢字、ひらがな、カタカナ以外では登録できない" do
+      end
+      it "first_nameは漢字、ひらがな、カタカナ以外では登録できない" do
+      end
+      it "last_name_kanaが空だと登録できない" do
+      end
+      it "first_name_kanaが空では登録できない" do
+      end
+      it "last_name_kanaはカタカナ以外では登録できない" do
+      end
+      it "first_name_kanaはカタカナ以外では登録できない" do
+      end
+      it " birthdayが空では登録できない" do
+      end
+    end
+  end
+
 end

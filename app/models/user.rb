440241bb3d7366f4750bc28708dfaf91c399283f
ayaokami <bbaqatb@gmail.com>
2022-01-10 17:21:33 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname,        presence: true
-  #パスワードの半角英数混合バリデーション
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください' 
-  #漢字/ひらがな/カタカナ入力のバリデーション
+
+  validates :nickname, presence: true
+  # パスワードの半角英数混合バリデーション
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください'
+  # 漢字/ひらがな/カタカナ入力のバリデーション
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
@@ -17,6 +17,5 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :last_name_kana
   end
-  validates :birthday,        presence: true
-       
+  validates :birthday, presence: true
 end

182b0de491783cc5044908264b1704302a59a405
ayaokami <bbaqatb@gmail.com>
2022-01-13 15:42:39 +0900

    ActiveHashのアソシエーション記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :ship_date
+  belongs_to :shipping_charges
 end

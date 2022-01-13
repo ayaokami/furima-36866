b3c9f7347f9752c8a6c2069c7609005fd82a93fe
ayaokami <bbaqatb@gmail.com>
2022-01-13 19:37:28 +0900

    newアクションの定義、form_withの記述、createアクションの定義、ストロングパラメータの定義
    ・カテゴリー選択の記述はコレクションセレクト内の記述後、ブラウザで表示されることを確認。
    ・ストロングパラメーターにはactivestorageのimageも追加。
    ・http://localhost:3000/items/newの画面でできる確認まで行い、DB保存は未確認。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_description, :category_id, :condition_id, :shipping_charges_id, :prefecture_id, :ship_date_id).merge(user_id: current_user.id)
+  end
 end

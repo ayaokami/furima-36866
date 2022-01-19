832a976c678c5ace0683ab2333e46c752f3a8452
ayaokami <bbaqatb@gmail.com>
2022-01-19 20:49:51 +0900

    ログイン時、他の人の商品を編集しようとするとトップページへ移動する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,6 +32,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update

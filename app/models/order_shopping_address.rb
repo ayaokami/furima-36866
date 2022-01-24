bea596d6496e5521306b52e589ebc20fa17f8ba5
ayaokami <bbaqatb@gmail.com>
2022-01-25 00:45:01 +0900

    バリデーションの実装

--- app/models/order_shopping_address.rb
+++ app/models/order_shopping_address.rb
@@ -1,4 +1,14 @@
 class OrderShoppingAddress
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :item_id
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :postcode,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "ハイフン(-)あり、半角数字で郵便番号を入力してください"}
+    validates :prefecture_id,  numericality: {other_than: 1, message: "都道府県を選んでください"}
+    validates :city
+    validates :block
+    validates :phone_number,  format: {with: /\A[0-9]{10,11}\z/, message: "ハイフン(-)なし、半角数字で電話番号を入力してください"}
+    validates :item_id
+    validates :user_id
+  end
 end

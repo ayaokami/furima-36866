ece1238f0c2381449298b52d7047749a93c0f663
ayaokami <bbaqatb@gmail.com>
2022-01-25 22:15:44 +0900

    2重ハッシュの外にあるものはmergeで紐づける
    permit内に:item_idと記載していたが、2重ハッシュの外にある値はorder_paramsで受け渡されず、空のバリデーションにかかって保存できずにいた。
    mergeに記載し、order_paramsに入れることができ、受け渡し成功！

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,6 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order_shopping_address = OrderShoppingAddress.new(order_params)
-    binding.pry
     if @order_shopping_address.valid?
       @order_shopping_address.save
       redirect_to root_path
@@ -18,6 +17,6 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order_shopping_address).permit(:item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
+    params.require(:order_shopping_address).permit( :postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end

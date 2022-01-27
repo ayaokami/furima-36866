0b271bf736a541288e4cfe30202365a5737ff3b0
ayaokami <bbaqatb@gmail.com>
2022-01-27 23:33:49 +0900

    購入ボタンclick時にイベント発火する

---
+++ app/javascript/card.js
@@ -0,0 +1,8 @@
+const pay = () => {
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("フォーム送信時にイベント発火")
+  });
+};
+window.addEventListener("load", pay);
